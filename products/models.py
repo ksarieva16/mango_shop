92b4dd1dfcc7aa1b7c518b8547cf881cbfb49adb
ksarieva16 <ksarieva16@gmail.com>
2022-07-18 10:36:50 +0600

    Add cart, favorites, comments, category

--- products/models.py
+++ products/models.py
@@ -1,24 +1,30 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from datetime import datetime
+from slugify import slugify
 
 User = get_user_model()
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256, blank=True, primary_key=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
-        ordering = ('title',)
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-
-    def __str__(self) -> str:
-        return self.title
+        verbose_name = 'Category'
+        verbose_name_plural = 'Сategories'
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=256)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category,
@@ -30,4 +36,55 @@ class Product(models.Model):
         return self.title
 
 
+class Comment(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'Comment from {self.author.name} to {self.product}'
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+        ordering = ['-created_at']
+
+
+# class Image(models.Model):
+#     clothes = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='clothes_image')
+#     image = models.ImageField(upload_to='products')
+#
+#     def __str__(self):
+#         return f'{self.clothes}'
+#
+#     class Meta:
+#         verbose_name = 'Image'
+#         verbose_name_plural = 'Images'
+
+
+class Like(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='like')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='like')
+    like = models.BooleanField(default=False, blank=True)
+
+    def __str__(self):
+        return f'{self.author.name} liked {self.product}'
+
+    class Meta:
+        verbose_name = 'like'
+        verbose_name_plural = 'Likes'
+
+
+class Favorites(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='favorites')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='favorites')
+    favorites = models.BooleanField(default=False, blank=True)
+
+    def __str__(self):
+        return f'{self.author.name} favorites {self.product}'
+
+    class Meta:
+        verbose_name = 'Favorite'
+        verbose_name_plural = 'Favorites'
--- products/permissions.py
+++ products/permissions.py
@@ -4,3 +4,8 @@ from rest_framework.permissions import BasePermission
 class IsAuthor(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and request.user == obj.author
+
+
+# class IsAuthorOrAdmin(BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         return bool(request.user == obj.user or request.user.is_staff)
