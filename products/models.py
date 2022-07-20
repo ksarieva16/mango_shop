3c8eef00ca80efc61257dea4b61199721b499df1
ksarieva16 <ksarieva16@gmail.com>
2022-07-20 11:39:23 +0600

    fixed

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from slugify import slugify
 from datetime import datetime
 
+User = get_user_model()
 
 class Category(models.Model):
     title = models.CharField(max_length=256)
@@ -22,6 +23,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
     title = models.CharField(max_length=256)
     description = models.TextField()
     # price = models.DecimalField(max_digits=10, decimal_places=2)
