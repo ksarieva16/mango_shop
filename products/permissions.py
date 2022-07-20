4245f736d911f30d751c5f6243eff7d4527dbcd4
ksarieva16 <ksarieva16@gmail.com>
2022-07-20 16:38:55 +0600

    Itog

--- products/permissions.py
+++ products/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsAuthorOrAdmin(BasePermission):
+class IsAuthor(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user == obj.user or request.user.is_staff
