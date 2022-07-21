ef49d400b6c0e0acbf7ff678a2a0b8eef9ece23e
ksarieva16 <ksarieva16@gmail.com>
2022-07-21 19:36:48 +0600

    Рабочая версия

--- mango_shop/settings.py
+++ mango_shop/settings.py
@@ -171,12 +171,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3000',
-#     'http://localhost:3001',
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:3001',
+]
 
 
 REST_FRAMEWORK = {
