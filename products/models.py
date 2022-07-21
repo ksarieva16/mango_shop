94afb505d1be8110d15b3b7a3a94a0bcb2772ee6
ksarieva16 <ksarieva16@gmail.com>
2022-07-21 21:51:38 +0600

    Рабочая версия

--- products/models.py
+++ products/models.py
@@ -29,7 +29,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user', blank=True, null=True)
     title = models.CharField(max_length=256)
     description = models.TextField()
     # price = models.DecimalField(max_digits=10, decimal_places=2)
